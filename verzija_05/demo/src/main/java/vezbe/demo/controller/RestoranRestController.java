c292015f5b9749629b02f129f6a09e6cd07dcbf0
sirarmilos <losmac256@gmail.com>
2022-05-15 12:39:11 +0200

    paragraf_2_verzija_2
    -Uradjeno:
    - nista nije uradjeno, zapoceto je:
    
    - api/restoran/pretraga
    - api/korisnik/izbor_restorana/{id}

--- verzija_05/demo/src/main/java/vezbe/demo/controller/RestoranRestController.java
+++ verzija_05/demo/src/main/java/vezbe/demo/controller/RestoranRestController.java
@@ -3,14 +3,14 @@ package vezbe.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import vezbe.demo.dto.PretragaRestoranaDto;
 import vezbe.demo.model.Restoran;
 import vezbe.demo.service.RestoranService;
 import vezbe.demo.service.SesijaService;
 
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -40,8 +40,6 @@ public class RestoranRestController {
             }
         }
 
-        System.out.println(povratna);
-
         if(povratna == false)
         {
             return new ResponseEntity("Ne mozete da vidite podatke o restoranima, zato sto niste nijedan tip korisnika. Ulogujte se.", HttpStatus.BAD_REQUEST);
@@ -50,6 +48,32 @@ public class RestoranRestController {
         List<Restoran> listaSvihRestorana = restoranService.PregledSvihRestorana();
 
         return new ResponseEntity(listaSvihRestorana, HttpStatus.OK);
+    }
+
+    @GetMapping("api/restoran/pretraga")
+    public ResponseEntity PretragaRestorana(@RequestBody PretragaRestoranaDto pretragaRestoranaDto)
+    {
+        HashMap<String, String> podaciGreske = new HashMap<>();
+
+        List<Restoran> trazeniRestorani = null;
+
+        try{
+            trazeniRestorani = restoranService.PretragaRestorana(pretragaRestoranaDto);
+        } catch (Exception e){
+            podaciGreske.put("Restorani", e.getMessage());
+        }
+
+        if(trazeniRestorani == null)
+        {
+            return new ResponseEntity(podaciGreske, HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity(trazeniRestorani, HttpStatus.OK);
+    }
+
+    @GetMapping("api/korisnik/izbor_restorana/{id}")
+    public ResponseEntity PrikazIzabranogRestorana(@PathVariable Long id)
+    {
+
     }
 
 }
--- verzija_05/demo/src/main/java/vezbe/demo/service/RestoranService.java
+++ verzija_05/demo/src/main/java/vezbe/demo/service/RestoranService.java
@@ -2,6 +2,7 @@ package vezbe.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import vezbe.demo.dto.PretragaRestoranaDto;
 import vezbe.demo.model.Porudzbina;
 import vezbe.demo.model.Restoran;
 import vezbe.demo.repository.PorudzbinaRepository;
@@ -33,6 +34,21 @@ public class RestoranService {
     public List<Restoran> PregledSvihRestorana()
     {
         return restoranRepository.findAll();
+    }
+
+    public List<Restoran> PretragaRestorana(PretragaRestoranaDto pretragaRestoranaDto) throws Exception {
+        //List<Restoran> restorani = restoranRepository.findAllByNazivContainsAndTipContainsAndLokacijaContains(pretragaRestoranaDto.getNaziv(), pretragaRestoranaDto.getTip(), pretragaRestoranaDto.getLokacija());
+        //List<Restoran> restorani = restoranRepository.findAllByNazivStartingWithAndTipStartingWith(pretragaRestoranaDto.getNaziv(), pretragaRestoranaDto.getTip());
+        List<Restoran> restorani = restoranRepository.findByTipLike(pretragaRestoranaDto.getTip());
+
+        System.out.println("JESTE");
+
+        if(restorani.isEmpty() == true)
+        {
+            throw new Exception("Ne postoji trazeni restoran");
+        }
+
+        return  restorani;
     }
 
 }
